c482a1c00581c2465ca277e2be45acbca615cd0b
Kurumi <minhneahihi123@gmail.com>
2026-03-09 21:42:53 +0700

    chỉnh sửa font chữ admin

--- src/components/layout/AdminLayout.tsx
+++ src/components/layout/AdminLayout.tsx
@@ -1,6 +1,7 @@
 import Sidebar from "./Sidebar";
 import { Outlet } from "react-router-dom";
 import "./sidebar.css";
+import "./admin-font.css";
 export default function MainLayout() {
   return (
     <div className="admin-layout">
